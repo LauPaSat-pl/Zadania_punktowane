465aca17e412443a4b39a0356560ef1b9a69b401
genejam <michal.geneja.2@stud.pw.edu.pl>
2022-03-31 19:31:28 +0200

    Final

--- zadanie1/src/Main.java
+++ zadanie1/src/Main.java
@@ -2,10 +2,11 @@ import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
-		Random random = new Random();
-		for (int i=0;i<100;i++) {
+		MineField mineField = new MineField();
+		Bozena mineFlail = new Bozena(mineField);
 
-			System.out.println(random.nextDouble());
+		for(int i=0;i<50;i++){
+			mineFlail.destroyMines(mineField);
 		}
 	}
 }
